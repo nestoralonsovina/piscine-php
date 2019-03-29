15aa95f5eb82c228d951708992d30560c4e9c0ea
Nestor Alonso <nalonso@student.42.fr>
2019-03-29 12:26:38 +0100

    added two first exercises of day04

---
+++ day04/ex00/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login']) && !isset($_SESSION['login'])) {
+    $_SESSION['login'] = "";
+    $_SESSION['passwd'] = "";
+}
+
+if (isset($_GET)) {
+    if (isset($_GET['submit']) && isset($_GET['login']) && isset($_GET['passwd'])) {
+        if ($_GET['submit'] == "OK") {
+            $_SESSION['login'] = $_GET['login'];
+            $_SESSION['passwd'] = $_GET['passwd'];
+        }
+    }
+}
+
+?>
+<html><body>
+<form method="get" action="index.php">
+    Username: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
+    <br />
+    Password: <input type="text" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
+    <input type="submit" name="submit" value="OK" />
+</form>
+</body></html>
